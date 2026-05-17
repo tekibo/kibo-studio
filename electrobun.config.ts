cf4cc6d67755ec375669da39bff2336e1e3a1c30
tekibo <tekibohelp@gmail.com>
2026-05-18 01:54:39 +0530

    0.0.2

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "kibo-studio",
 		identifier: "kibostudio.tekibo.in",
-		version: "0.0.1",
+		version: "0.0.2",
 	},
 	build: {
 		// Vite builds to dist/, we copy from there
